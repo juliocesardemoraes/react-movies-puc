a18cd038e3882c7efb212b874b11699db799fd9f
julio cesar <jcmcf2012@gmail.com>
2023-04-23 10:51:10 -0300

    feat: add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Landingpage from "./components/pages/Landingpage.tsx";
+import Moviedetail from "./components/pages/Moviedetail.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landingpage></Landingpage>,
+  },
+  {
+    path: "/movie/:movieId",
+    element: <Moviedetail></Moviedetail>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
